0fb205cf6e8d5b6766a38d4b941bd8d7b4cce903
Perpetue <muperpety@gmail.com>
2019-01-19 13:16:02 +0200

    add all changes in script.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,50 @@
-$(document).ready(function() {
-//busines logical
-    $("form#new-account").submit(function(event) {
-      event.preventDefault();
-      $(".withdraw").each(function() {
-        var inputtedFullname  = $(this).find("input#new-full-name").val();
-        var inputtedNewDeposit = $(this).find("input#new-deposit").val();
-        var newAccount = new Account(inputtedDepositAmount, inputtedWithdraw)
-        newaccount.withdraw.push(newAccount)
+
+function BankAccount(name, initialDeposit, balance) {
+  this.userName = name;
+  this.firstDeposit = initialDeposit;
+  this.balance = balance;
+}
+//method for create account
+
+
+
+//method for adding a deposit.
+BankAccount.prototype.makeDeposit = function(deposit) {
+  this.balance += deposit;
+}
+//method for substracting a withdrawl.
+BankAccount.prototype.makeWithdrawl = function(withdrawl) {
+  this.balance -= withdrawl;
+}
+
+//User interface logic
+$(document).ready(function(){
+  $('form#submit_new_account').submit(function(event) {
+    event.preventDefault();
+   var  inputtedFullName = $("input#user_name").val();
+   var  inputedID = $('#user_id').val();
+   var  name = $("input#user_name").val();
+     initialDeposit = parseInt($('#initial_deposit').val());
+     newBalance = initialDeposit;
+     userAccount = new BankAccount(name, initialDeposit, newBalance);
+    console.log(userAccount);
+    $('.new_account').remove();
+    $('.balance_here').text(`$${userAccount.balance}`);
+
+      $('#make_deposit').submit(function(event) {
+        event.preventDefault();
+         deposit = parseInt($('#current_deposit').val());
+        userAccount.makeDeposit(deposit);
+        $('.balance_here').text(`${userAccount.balance}`);
+        $('#make_deposit').trigger('reset');
       });
-      var inputtedDepositAmount= $("input.deposit-amount").val();
-      var inputtedWithdraw = $("input.Withdraw").val();
-      var newCustomer = new Customer(inputtedFullname, inputtedNewDeposit);
-      $("#balance").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
-    });
-  });
+
+      $('#make_withdrawl').submit(function(event) {
+        event.preventDefault();
+         withdrawl = parseInt($('#current_withdrawl').val());
+        userAccount.makeWithdrawl(withdrawl);
+        $('.balance_here').text(`${userAccount.balance}`);
+        $('#make_withdrawl').trigger('reset');
+      })
+  })
+})
